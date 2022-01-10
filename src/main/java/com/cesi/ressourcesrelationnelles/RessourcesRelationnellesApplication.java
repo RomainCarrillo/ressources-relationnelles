9f6ba76415db00ffb4d2ce625a81aec2cf58f909
Romain Carrillo <carrillo.romain@gmail.com>
2022-01-10 16:30:58 +0100

    Add hello controller

--- src/main/java/com/cesi/ressourcesrelationnelles/RessourcesRelationnellesApplication.java
+++ src/main/java/com/cesi/ressourcesrelationnelles/RessourcesRelationnellesApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RessourcesRelationnellesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RessourcesRelationnellesApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RessourcesRelationnellesApplication.class, args);
+    }
 }
