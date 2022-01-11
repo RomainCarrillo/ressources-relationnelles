f8b40a5ab9e8359cd81fa8484d7d3ca899cdb5d8
Romain Carrillo <carrillo.romain@gmail.com>
2022-01-11 14:30:11 +0100

    Add comment class

---
+++ src/main/java/com/cesi/ressourcesrelationnelles/features/comments/CommentClass.java
@@ -0,0 +1,11 @@
+package com.cesi.ressourcesrelationnelles.features.comments;
+
+import java.util.Date;
+
+public class CommentClass {
+    public int id;
+    public Date date;
+    public String author;
+    public String title;
+    public String content;
+}
