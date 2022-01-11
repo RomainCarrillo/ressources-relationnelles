404d7bbfdf36e4b09c7317ac166dd1bffff19f2c
Augustin <damourette.augustin@gmail.com>
2022-01-11 12:11:52 +0100

    Ajout D'un ResourcesController

---
+++ src/main/java/com/cesi/ressourcesrelationnelles/features/resources/ResourcesController.java
@@ -0,0 +1,14 @@
+package com.cesi.ressourcesrelationnelles.features.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourcesController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Resources Controller emplacement ";
+    }
+
+}
