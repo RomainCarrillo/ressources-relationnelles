93305c213dfd0104c9ca4ad8a888fc06a657dedd
Alexandre MAINDRON <alexandre.maindron@viacesi.fr>
2022-01-12 10:20:24 +0100

    feat: added return of a mocked User List

--- src/main/java/com/cesi/ressourcesrelationnelles/features/users/UsersController.java
+++ src/main/java/com/cesi/ressourcesrelationnelles/features/users/UsersController.java
@@ -1,13 +1,21 @@
 package com.cesi.ressourcesrelationnelles.features.users;
 
+import com.cesi.ressourcesrelationnelles.domain.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UsersController {
 
     @GetMapping("/users")
-    public String index() {
-        return "Welcome user !";
+    public List<User> index() {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User(0,"alexandre.maindron@viacesi.fr", "123456789", "Alexandre", "MAINDRON"));
+        userList.add(new User(1,"albert@gmail.com", "123", "Albert", "CAMUS"));
+        userList.add(new User(2,"jacquie@gouv.fr", "15", "Jacques", "CHIRAC"));
+        return userList;
     }
 }
