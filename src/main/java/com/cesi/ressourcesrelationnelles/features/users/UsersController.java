003bfbe1dfa253236b0cb28b2f531dd7f14a8cd5
Alexandre MAINDRON <alexandre.maindron@viacesi.fr>
2022-01-13 11:11:31 +0100

    feat: add ability to delete a User by his ID

--- src/main/java/com/cesi/ressourcesrelationnelles/features/users/UsersController.java
+++ src/main/java/com/cesi/ressourcesrelationnelles/features/users/UsersController.java
@@ -2,10 +2,7 @@ package com.cesi.ressourcesrelationnelles.features.users;
 
 import com.cesi.ressourcesrelationnelles.domain.User;
 import com.cesi.ressourcesrelationnelles.services.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,10 @@ public class UsersController {
     @PostMapping("/createuser")
     public User createUser(@RequestBody User user){
         return userService.createUser(user);
+    }
+
+    @DeleteMapping("/deleteuser/{id}")
+    public void deleteUser(@PathVariable long id){
+        userService.deleteUser(id);
     }
 }
