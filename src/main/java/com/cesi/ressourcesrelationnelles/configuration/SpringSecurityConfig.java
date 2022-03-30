e4d140171a8597e0e7f2d0fad9e3c1104811e125
Romain Carrillo <carrillo.romain@gmail.com>
2022-03-30 23:09:30 +0200

    Add configure() methods

--- src/main/java/com/cesi/ressourcesrelationnelles/configuration/SpringSecurityConfig.java
+++ src/main/java/com/cesi/ressourcesrelationnelles/configuration/SpringSecurityConfig.java
@@ -1,17 +1,42 @@
 package com.cesi.ressourcesrelationnelles.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	//Attention, l'ordre des méthode et des filtres est important
+	
+	//TODO replace in code users and admin informations with DB informations
 	@Override
-	public void configure(HttpSecurity http) throws Exception{
-		
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+			.withUser("user").password(passwordEncoder().encode("user123")).roles("USER")
+			.and()
+			.withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN", "USER");
+
 	}
 	
+	@Override
+	public void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+			.antMatchers("/admin").hasRole("ADMIN")
+			.antMatchers("/user").hasRole("USER")
+			.anyRequest().authenticated()
+			.and()
+			.formLogin();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
